5f650d8159a92cecd52b34a7e21d160c2b4e85f8
claudio <611claudio@gmail.com>
2026-09-01 18:32:31 -0300

    Comunicação de backend para front é feita(ações necessário para aplicativo não é feita, somente ação para teste é feita). O efeito pode ser visto no Public_html/html/test.html

--- ACEX_project/WEB/Public_html/testawawa.php
+++ ACEX_project/WEB/Public_html/testawawa.php
@@ -1,14 +1,17 @@
-<?php 
-    echo "email:" . $_POST['email'];
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastre-se</title>
+    <title>wawawa</title>
 </head>
+<h1>no ambiente real, você deve usar fetch para pegar esse informação com method 'GET' invéz de usar php.</h1>
+<h1>O uso de php aqui só tem fim de demonstração!!!.</h1>
+<?php 
+    echo "email:" . $_POST['email'];
+?>
+
+
 <body>
 
 <p>hi</p>
